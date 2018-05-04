5aa240140f9cbf34d467864fb8ed8c7e233a173a
Noctaum <Noctaum@users.noreply.github.com>
2018-05-04 12:12:37 +0300

    deleted empty comments

--- server.js
+++ server.js
@@ -18,41 +18,6 @@ app.use(films);
 app.use(users);
 app.use(files);
 
-//////////////ALL FOR FIRST TABLE//////////////
-
-// for(let i = 0; i < 100; i++){
-// var newFilm = new films({
-//     Name: "Law Abiding Citizen",
-//     year: 2009,
-//     author: "F. Gary Gray",
-//     category: 1,
-//     text: "A frustrated man decides to take justice into his own hands after a plea bargain sets one of his family's killers free. He targets not only the killer but also the district attorney and others involved in the deal.",
-// });
-// newFilm.save();
-// }
-
-
-
-//////////////ALL FOR SECOND TABLE//////////////
-
-
-
-// for(let i = 0; i < 100; i++){
-// var secondUser = new users2({
-//     firstName: "William",
-//     secondName: "Terner",
-//     text: "I'm artist.",
-//     value: "44",
-// });
-// secondUser.save();
-// }
-
-
-
-//////////////ALL FOR THIRD TABLE (Settings)//////////////
-
-
-
 app.listen(8096, function(){
     console.log("Started!");
 });
